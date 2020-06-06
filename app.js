7c8460f8d2d2f659b9785a9e09d5d4affb2514ae
Karolina Jasiak <kjasiak1993@gmail.com>
2020-06-06 14:43:31 +0200

    Inicjalizacja silnika szablonów Handlebars

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
 const handlebars = require('express-handlebars');
+
+const host = '127.0.0.1';
+const port = 2500;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+  layoutsDir: __dirname + '/views/layouts',
+  extname: 'hbs',
+  defaultLayout: 'index',
+  partialsDir: __dirname + '/views/partials/'
+}));
+
+app.use(express.static('public'));
